b4e950ea87e23707b92dc5d8791aed1df8952471
hamanok99 <kazuki.hamano@wisdom.co.jp>
2021-11-02 18:51:53 +0900

    composer追加

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2baffeddbdc488a031b72619d5789c0a9122cc40',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2baffeddbdc488a031b72619d5789c0a9122cc40',
+            'dev_requirement' => false,
+        ),
+    ),
+);
