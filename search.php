72c874470084602586b99311dc492e4268cf2fba
hamanok99 <kazuki.hamano@wisdom.co.jp>
2021-11-02 17:42:11 +0900

    修正

--- search.php
+++ search.php
@@ -54,15 +54,14 @@ echo '<table border="1">
     </tr>';
 
     // data.csv（SJIS）を読み込む
-    $sjis_data = file_get_contents('data.csv');
-    var_dump($sjis_data);
-    var_dump(mb_convert_encoding($sjis_data, 'UTF-8', 'SJIS'));
-    // 一時ファイル作成
-    $fp = tmpfile();
-    // UTF-8に変換して一時ファイルに書き込み
-    fwrite($fp, mb_convert_encoding($sjis_data, 'UTF-8', 'SJIS'));
-    // ポインタを先頭に
-    fseek($fp, 0);
+    $fp = fopen('data.csv', 'r');
+
+    while($data = fgetcsv($fp))
+    {
+        var_dump($data);
+    }
+
+    fclose($fp);
 
     // テーブルの閉じタグ
     echo '</table>';
