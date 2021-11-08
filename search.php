b3e0206ec4c344a247b4220ed847f2d6867abf4d
hamanok99 <kazuki.hamano@wisdom.co.jp>
2021-11-08 17:52:43 +0900

    修正

--- search.php
+++ search.php
@@ -97,11 +97,7 @@
                         echo "<td>" . $data[$i] . "</td>";
                     }
                     echo '</tr>';
-                    }else if (isset($_POST["name"], $_POST["age"], $_POST["gender"],
-                    $_POST["address"], $_POST["telephone"], $_POST["mail"], $_POST["thoughts"])) {
-                              {
-                                  echo "setOK";
-                                  echo $_POST['name'] . $_POST['age'] . $_POST['gender'] . $_POST['address'] . $_POST['telephone'] . $_POST['mail'] . $_POST['thoughts'];
+                    } else if (isset($_POST["name"], $_POST["age"], $_POST["gender"],$_POST["address"], $_POST["telephone"], $_POST["mail"], $_POST["thoughts"])) {
                                 if (empty($_POST['name'] &&
                                     $_POST['age'] &&
                                     $_POST['gender'] &&
@@ -116,7 +112,6 @@
                                         echo "<td>" . $data[$i] . "</td>";
                                     }
                                     echo '</tr>';
-                                }
                     }
                 }
                  // テーブルの閉じタグ
