1c157997aa073a5ce007789d4889bf8f8a92f6c6
hamanok99 <kazuki.hamano@wisdom.co.jp>
2021-11-08 18:06:02 +0900

    修正

--- search.php
+++ search.php
@@ -96,8 +96,9 @@
                         echo "<td>" . $data[$i] . "</td>";
                     }
                     echo '</tr>';
-                    }else if (isset($_POST["name"]))
-                              {
+                    }else if (isset($_POST["name"], $_POST["age"], $_POST["gender"],
+                    $_POST["address"], $_POST["telephone"], $_POST["mail"], $_POST["thoughts"]))
+                    {
                                   echo "setOK";
                                   echo $_POST['name'] . $_POST['age'] . $_POST['gender'] . $_POST['address'] . $_POST['telephone'] . $_POST['mail'] . $_POST['thoughts'];
                                   if(empty($_POST['name'] && $_POST['age'] && $_POST['gender'] &&
