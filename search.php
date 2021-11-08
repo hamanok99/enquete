44d878047ada234141d25498409b6634d2ecf8d3
hamanok99 <kazuki.hamano@wisdom.co.jp>
2021-11-08 15:39:27 +0900

    修正

--- search.php
+++ search.php
@@ -72,6 +72,7 @@
                     <td><input class="contact-submit" type="submit" value="検索" /></td>
                 </tr>
             </table>
+            <?php echo $_POST['thoughts'] ?>
             <table>
                 <tr>
                     <th>氏名</th>
@@ -88,6 +89,7 @@
                 // while文でCSVファイルのデータを1つずつ繰り返し読み込む
                 while($data = fgetcsv($fp)){
                     mb_convert_variables("UTF-8", "SJIS-win", $data);
+                    echo $data[6];
 
                     if($data[0]===$_POST['name'] || $data[1]===$_POST['age'] || $data[2]===$_POST['gender'] || $data[3]===$_POST['address']
                     || $data[4]===$_POST['telephone'] || $data[5]===$_POST['mail'] || $data[6]===$_POST['thoughts']){
