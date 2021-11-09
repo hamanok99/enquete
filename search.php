0aa26af7cccfcd6da7317355ca874279bb5fb474
hamanok99 <kazuki.hamano@wisdom.co.jp>
2021-11-09 17:39:11 +0900

    修正

--- search.php
+++ search.php
@@ -113,53 +113,60 @@
                             }
                             echo '</tr>';
                         }
-                    }
-                    //入力項目と登録項目が完全一致の場合一覧表示
-                    else
-                    {
-                        echo '<tr>';
-                        if(strpos($data[0], $NAME) !== false){
-                            echo '<td>' . $data[0] . '</td>';
-                        } else {
-                            echo '<td></td>';
-                        }
+                        //入力項目と登録項目が完全一致の場合一覧表示
+                        else
+                        {
+                            echo '<tr>';
+                            if(strpos($data[0], $NAME) !== false){
+                                echo 'name一致<br/>';
+                                echo '<td>' . $data[0] . '</td>';
+                            } else {
+                                echo '<td></td>';
+                            }
 
-                        if($data[1] === $AGE){
-                            echo '<td>' . $data[1] . '</td>';
-                        } else {
-                            echo '<td></td>';
-                        }
+                            if($data[1] === $AGE){
+                                echo 'age一致<br/>';
+                                echo '<td>' . $data[1] . '</td>';
+                            } else {
+                                echo '<td></td>';
+                            }
 
-                        if(strpos($data[2], $GENDER) !== false){
-                            echo '<td>' . $data[2] . '</td>';
-                        } else {
-                            echo '<td></td>';
-                        }
+                            if(strpos($data[2], $GENDER) !== false){
+                                echo 'gender一致<br/>';
+                                echo '<td>' . $data[2] . '</td>';
+                            } else {
+                                echo '<td></td>';
+                            }
 
-                        if(strpos($data[3], $ADDRESS) !== false){
-                            echo '<td>' . $data[3] . '</td>';
-                        } else {
-                            echo '<td></td>';
-                        }
+                            if(strpos($data[3], $ADDRESS) !== false){
+                                echo 'address一致<br/>';
+                                echo '<td>' . $data[3] . '</td>';
+                            } else {
+                                echo '<td></td>';
+                            }
 
-                        if(strpos($data[4], $TELEPHONE) !== false){
-                            echo '<td>' . $data[4] . '</td>';
-                        } else {
-                            echo '<td></td>';
-                        }
+                            if(strpos($data[4], $TELEPHONE) !== false){
+                                echo 'tel一致<br/>';
+                                echo '<td>' . $data[4] . '</td>';
+                            } else {
+                                echo '<td></td>';
+                            }
 
-                        if(strpos($data[5], $MAIL) !== false){
-                            echo '<td>' . $data[5] . '</td>';
-                        } else {
-                            echo '<td></td>';
-                        }
+                            if(strpos($data[5], $MAIL) !== false){
+                                echo 'mail一致<br/>';
+                                echo '<td>' . $data[5] . '</td>';
+                            } else {
+                                echo '<td></td>';
+                            }
 
-                        if(strpos($data[6], $THOUGHTS) !== false){
-                            echo '<td>' . $data[6] . '</td>';
-                        } else {
-                            echo '<td></td>';
+                            if(strpos($data[6], $THOUGHTS) !== false){
+                                echo 'TH一致<br/>';
+                                echo '<td>' . $data[6] . '</td>';
+                            } else {
+                                echo '<td></td>';
+                            }
+                            echo '</tr>';
                         }
-                        echo '</tr>';
                     }
                 }
 
