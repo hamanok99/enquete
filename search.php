3b4789d38b1ebed5a40ce3d711bdd3811d30c162
hamanok99 <kazuki.hamano@wisdom.co.jp>
2021-11-09 18:47:26 +0900

    修正

--- search.php
+++ search.php
@@ -104,78 +104,70 @@
                     //UTF-8に変換
                     mb_convert_variables("UTF-8", "SJIS-win", $data);
 
-                    // 全ての条件が空白の場合
-                    if (isset($_GET["name"], $_GET["age"], $_GET["gender"], $_GET["address"], $_GET["telephone"], $_GET["mail"], $_GET["thoughts"])) {
-                        if(empty($_GET['name']) && empty($_GET['age']) && empty($_GET['gender']) && empty($_GET['address']) && empty($_GET['telephone']) && empty($_GET['mail']) && empty($_GET['thoughts'])){
+                    // 検索用フラグ
+                    $display = True;
+
+                    // 検索条件を取得
+                    $get = $_GET;
+
+                    // 検索条件がある場合
+                    if (!empty($get)){
+                        // nameが入力されている
+                        if(empty($NAME) === false) {
+                            // 部分一致しない
+                            if ((strpos($data[0], $NAME) === false)){
+                                $display = false;
+                            }
+                        }
+                        // 年齢が入力されている
+                        if (empty($AGE) === false) {
+                            // 完全一致しない
+                            if (($data[1] !== $AGE)){
+                                $display = false;
+                            }
+                        }
+                        // 性別が入力されている
+                        if (empty($GENDER) === false) {
+                            // 完全一致しない
+                            if ($data[2] !== $GENDER){
+                                $display = false;
+                            }
+                        }
+                        // 住所が入力されている
+                        if (empty($ADDRESS) === false) {
+                            // 部分一致しない
+                            if (strpos($data[3], $ADDRESS) === false){
+                                $display = false;
+                            }
+                        }
+                        // 電話番号が入力されている
+                        if (empty($TELEPHONE) === false) {
+                            // 部分一致しない
+                            if (strpos($data[4], $TELEPHONE) === false) {
+                                $display = false;
+                            }
+                        }
+                        // メールが入力されている
+                        if (empty($MAIL) === false) {
+                            // 部分一致しない
+                            if (strpos($data[5], $MAIL) === false) {
+                                $display = false;
+                            }
+                        }
+                        // 感想が入力されている
+                        if (empty($THOUGHTS) === false) {
+                            // 完全一致する
+                            if ($data[6] === $THOUGHTS) {
+                                $display = false;
+                            }
+                        }
+                        if ($display === True)
+                        {
                             echo '<tr>';
                             for ($i=0;$i<count($data);$i++) {
-                            echo "<td>" . $data[$i] . "</td>";
+                                echo "<td>" . $data[$i] . "</td>";
                             }
                             echo '</tr>';
-                        }
-                        //入力項目と登録項目が完全一致の場合一覧表示
-                        else
-                        {
-                            // 検索用フラグ
-                            $display = True;
-
-                            // nameが入力されている
-                            if(empty($NAME) === false) {
-                                // 部分一致しない
-                                if ((strpos($data[0], $NAME) === false)){
-                                    $display = false;
-                                }
-                            }
-                            // 年齢が入力されている
-                            if (empty($AGE) === false) {
-                                // 完全一致しない
-                                if (($data[1] !== $AGE)){
-                                    $display = false;
-                                }
-                            }
-                            // 性別が入力されている
-                            if (empty($GENDER) === false) {
-                                // 完全一致しない
-                                if ($data[2] !== $GENDER){
-                                    $display = false;
-                                }
-                            }
-                            // 住所が入力されている
-                            if (empty($ADDRESS) === false) {
-                                // 部分一致しない
-                                if (strpos($data[3], $ADDRESS) === false){
-                                    $display = false;
-                                }
-                            }
-                            // 電話番号が入力されている
-                            if (empty($TELEPHONE) === false) {
-                                // 部分一致しない
-                                if (strpos($data[4], $TELEPHONE) === false) {
-                                    $display = false;
-                                }
-                            }
-                            // メールが入力されている
-                            if (empty($MAIL) === false) {
-                                // 部分一致しない
-                                if (strpos($data[5], $MAIL) === false) {
-                                    $display = false;
-                                }
-                            }
-                            // 感想が入力されている
-                            if (empty($THOUGHTS) === false) {
-                                // 完全一致する
-                                if ($data[6] === $THOUGHTS) {
-                                    $display = false;
-                                }
-                            }
-                            if ($display === True)
-                            {
-                                echo '<tr>';
-                                for ($i=0;$i<count($data);$i++) {
-                                    echo "<td>" . $data[$i] . "</td>";
-                                }
-                                echo '</tr>';
-                            }
                         }
                     }
                 }
